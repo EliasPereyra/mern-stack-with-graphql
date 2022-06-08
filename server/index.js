4ccc1d613a5d1f365aedba13184e102d3479618f
EliasPereyra <EliasPereyra_Gomez@hotmail.com>
2022-06-08 09:49:13 -0300

    Server: connecting to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const colors = require("colors");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
+
 const schema = require("./schema/schema");
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// Connect to DB
+connectDB();
 
 app.use(
   "/graphql",
