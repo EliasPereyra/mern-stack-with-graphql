bbe9af87227a48aa763f439f50adcd48553f485e
EliasPereyra <EliasPereyra_Gomez@hotmail.com>
2022-06-08 00:20:20 -0300

    Server: graphql queries for the projects

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,7 +9,6 @@ const {
 } = require("graphql");
 
 // Client Type
-
 const ClientType = new GraphQLObjectType({
   name: "Client",
   fields: () => ({
@@ -20,9 +19,33 @@ const ClientType = new GraphQLObjectType({
   }),
 });
 
+// Projects
+const ProjectType = new GraphQLObjectType({
+  name: "Project",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+  }),
+});
+
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return projects;
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return projects.find((project) => project.id === args.id);
+      },
+    },
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args) {
