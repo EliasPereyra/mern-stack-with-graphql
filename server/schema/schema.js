0cf24d6b1ed38300fc860176f4a715a09b4d5810
EliasPereyra <EliasPereyra_Gomez@hotmail.com>
2022-06-09 22:04:57 -0300

    Server: deleting a project

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -131,6 +131,15 @@ const mutation = new GraphQLObjectType({
         return project.save();
       },
     },
+    deleteProject: {
+      type: ProjectType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, args) {
+        return Project.findByIdAndRemove(args.id);
+      },
+    },
   },
 });
 
