3d09445c448ee583e347907ff1585039b803c2be
EliasPereyra <EliasPereyra_Gomez@hotmail.com>
2022-06-09 22:13:59 -0300

    Server: updating a project

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -140,6 +140,37 @@ const mutation = new GraphQLObjectType({
         return Project.findByIdAndRemove(args.id);
       },
     },
+    updateProject: {
+      type: ProjectType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: {
+          type: new GraphQLEnumType({
+            name: "ProjectStatusUpdate",
+            values: {
+              New: { value: "Not Started" },
+              Progress: { value: "In Progress" },
+              Completed: { value: "Completed" },
+            },
+          }),
+        },
+      },
+      resolve(parent, args) {
+        return Project.findByIdAndUpdate(
+          args.id,
+          {
+            $set: {
+              name: args.name,
+              description: args.description,
+              status: args.status,
+            },
+          },
+          { new: true }
+        );
+      },
+    },
   },
 });
 
