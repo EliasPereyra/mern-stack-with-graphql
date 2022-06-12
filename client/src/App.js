01e01ab2dda4c87202c361c98d15870286da1c72
EliasPereyra <EliasPereyra_Gomez@hotmail.com>
2022-06-12 10:34:25 -0300

    Client: deleting a client user

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
 import { Header, Clients } from "./components/";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql",
-  cache: new InMemoryCache(),
+  cache,
 });
 
 function App() {
