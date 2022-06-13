e1317fc4404d6aaa767246d4a52432d3c7a9f570
EliasPereyra <EliasPereyra_Gomez@hotmail.com>
2022-06-13 10:44:27 -0300

    Client: getting clients for select

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,4 +4,5 @@ export { default as ClientRow } from "./ClientRow";
 export { default as ClientInfo } from "./ClientInfo";
 export { default as Spinner } from "./Spinner";
 export { default as AddClientModal } from "./AddClientModal";
+export { default as AddProjectModal } from "./AddProjectModal";
 export { default as Projects } from "./Projects";
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { AddClientModal, Clients, Projects } from "../components";
+import {
+  AddClientModal,
+  AddProjectModal,
+  Clients,
+  Projects,
+} from "../components";
 
 function Home() {
   return (
     <>
       <div className=" gap-3 mb-4">
         <AddClientModal />
+        <AddProjectModal />
         <hr />
         <Projects />
         <hr />
