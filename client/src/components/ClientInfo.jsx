202eb4dd06d077c30965166548122c9b080f3a59
EliasPereyra <EliasPereyra_Gomez@hotmail.com>
2022-06-13 10:21:43 -0300

    Client: structuring the ClientInfo component

--- client/src/components/ClientInfo.jsx
+++ client/src/components/ClientInfo.jsx
@@ -1,9 +1,25 @@
-import { FaEnvelope, FaPhone, FaIdBadge } from "react-icons/fa"
+import { FaEnvelope, FaPhone, FaIdBadge } from "react-icons/fa";
 
 function ClientInfo({ client }) {
   return (
-    <div>ClientInfo</div>
-  )
+    <>
+      <h5 className="mt-5">Client Information</h5>
+      <ul className="list-group">
+        <li className="list-group-item">
+          <FaIdBadge className="icon" />
+          {client.name}
+        </li>
+        <li className="list-group-item">
+          <FaEnvelope className="icon" />
+          {client.email}
+        </li>
+        <li className="list-group-item">
+          <FaPhone className="icon" />
+          {client.phone}
+        </li>
+      </ul>
+    </>
+  );
 }
 
-export default ClientInfo
+export default ClientInfo;
--- client/src/queries/projectQueries.js
+++ client/src/queries/projectQueries.js
@@ -11,18 +11,20 @@ const GET_PROJECTS = gql`
 `;
 
 const GET_PROJECT = gql`
-query getProject($id: ID!) {
-  project(id: $id) {
-    id
-    name
-    description
-    status
-    client {
+  query getProject($id: ID!) {
+    project(id: $id) {
       id
       name
-      email
-      phone
+      description
+      status
+      client {
+        id
+        name
+        email
+        phone
+      }
+    }
   }
-}`;
+`;
 
 export { GET_PROJECTS, GET_PROJECT };
