8dd8711f658b810ab96876634376df3a119e28e3
EliasPereyra <EliasPereyra_Gomez@hotmail.com>
2022-06-14 11:35:47 -0300

    Client: adding a project

--- client/src/components/AddProjectModal.jsx
+++ client/src/components/AddProjectModal.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { FaList } from "react-icons/fa";
 import { useMutation, useQuery } from "@apollo/client";
 
+import { ADD_PROJECT } from "../mutations/projectMutations";
 import { GET_CLIENTS } from "../queries/clientQueries";
 import { GET_PROJECTS } from "../queries/projectQueries";
 import Spinner from "./Spinner";
@@ -12,6 +13,17 @@ function AddProjectModal() {
   const [clientId, setClientId] = useState("");
   const [status, setStatus] = useState("new");
 
+  const [addProject] = useMutation(ADD_PROJECT, {
+    variables: { name, description, clientId, status },
+    update(cache, { data: { addProject } }) {
+      const { projects } = cache.readQuery({ query: GET_PROJECTS });
+      cache.writeQuery({
+        query: GET_PROJECTS,
+        data: { projects: [...projects, addProject] },
+      });
+    },
+  });
+
   const { loading, error, data } = useQuery(GET_CLIENTS);
 
   const onSubmit = (e) => {
@@ -20,6 +32,8 @@ function AddProjectModal() {
     if ([name, description, status] === "") {
       return alert("Please, fill in all fields");
     }
+
+    addProject(name, description, clientId, status);
 
     setName("");
     setDescription("");
@@ -40,7 +54,7 @@ function AddProjectModal() {
             data-bs-toggle="modal"
             data-bs-target="#addProjectModal"
           >
-            <div className="d-flex align-">
+            <div className="d-flex align-items-center">
               <FaList className="icon" />
               <div>NewProject</div>
             </div>
@@ -95,9 +109,9 @@ function AddProjectModal() {
                         value={status}
                         onChange={(e) => setStatus(e.target.value)}
                       >
-                        <option value="new">Not Started</option>
-                        <option value="progress">In Progress</option>
-                        <option value="completed">Completed</option>
+                        <option value="New">Not Started</option>
+                        <option value="Progress">In Progress</option>
+                        <option value="Completed">Completed</option>
                       </select>
                     </div>
 
