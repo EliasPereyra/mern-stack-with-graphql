72cc7fa40f7c13f1060d3e66f1da5976d697b2a1
EliasPereyra <EliasPereyra_Gomez@hotmail.com>
2022-06-14 23:10:23 -0300

    Client: deleting a client and all the projects the client owned

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -3,21 +3,29 @@ import { useMutation } from "@apollo/client";
 
 import { DELETE_CLIENT } from "../mutations/clientMutations";
 import { GET_CLIENTS } from "../queries/clientQueries";
+import { GET_PROJECTS } from "../queries/projectQueries";
 
 function ClientRow({ client }) {
   const [deleteClient] = useMutation(DELETE_CLIENT, {
     variables: { id: client.id },
-    update(cache, { data: { deleteClient } }) {
-      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+    refetchQueries: [
+      { query: GET_CLIENTS },
+      {
+        query: GET_PROJECTS,
+      },
+    ],
+    // update(cache, { data: { deleteClient } }) {
+    //   const { clients } = cache.readQuery({ query: GET_CLIENTS });
 
-      cache.writeQuery({
-        query: GET_CLIENTS,
-        data: {
-          clients: clients.filter((client) => client.id !== deleteClient.id),
-        },
-      });
-    },
+    //   cache.writeQuery({
+    //     query: GET_CLIENTS,
+    //     data: {
+    //       clients: clients.filter((client) => client.id !== deleteClient.id),
+    //     },
+    //   });
+    // },
   });
+
   return (
     <tr>
       <td>{client.name}</td>
